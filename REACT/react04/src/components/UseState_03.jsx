5ac4ef205211ad1d95bcfe36a58d56ce05e00ed4
Dhymas Gabriel <dhymas.g@gmail.com>
2023-06-29 12:20:37 -0300

    Update UseState_03.jsx

--- REACT/react04/src/components/UseState_03.jsx
+++ REACT/react04/src/components/UseState_03.jsx
@@ -2,11 +2,39 @@ import { useState } from "react"
 
 export default () => {
 
-    
+    const [valor1, setValor1] = useState(1);
+    const [valor2, setValor2] = useState(2);
+
+    function diminuir1(){
+        setValor1(v => v - 1);
+    }
+
+    function aumentar1(){
+        setValor1(v => v + 1)
+    }
+
+    function diminuir2(){
+        setValor2(v => v - 1);
+    }
+
+    function aumentar2(){
+        setValor2(v => v + 1)
+    }
+
+
     return (
         <>
             <h2>UseState - Exercicio 1</h2>
-            
+
+            <p>{valor1}</p>
+            <button onClick={diminuir1}>-</button>
+            <button onClick={aumentar1}>+</button>
+            <hr />
+            <p>{valor2}</p>
+            <button onClick={diminuir2}>-</button>
+            <button onClick={aumentar2}>+</button>
+            <hr />
+            <p>O Resultado de {valor1} x {valor2} = {valor1 * valor2}</p>
         </>
     )
 }
