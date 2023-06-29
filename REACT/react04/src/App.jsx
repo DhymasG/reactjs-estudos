cffdddf8414ac00d26f6deafcd027da337b5c00f
Dhymas Gabriel <dhymas.g@gmail.com>
2023-06-29 15:38:46 -0300

    att

--- REACT/react04/src/App.jsx
+++ REACT/react04/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-import HookSate01 from './components/UseState_01'
+import HookState05 from './components/UseState_05'
 
 export default () => {
  
     return(
         <>
             <h1>React Hooks</h1>
-            <HookSate01 />
+            <HookState05 />
         </>
     )
 }
